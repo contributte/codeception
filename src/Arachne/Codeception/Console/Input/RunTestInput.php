cef218e97548ddd7629ccc09d6e08d7a94482fcc
Jáchym Toušek <enumag@gmail.com>
2014-09-03 21:30:39 +0200

    Improved RunTestInput class

--- src/Arachne/Codeception/Console/Input/RunTestInput.php
+++ src/Arachne/Codeception/Console/Input/RunTestInput.php
@@ -20,14 +20,30 @@ class RunTestInput extends ArgvInput
 		$parameters = array($_SERVER['argv'][0], 'run');
 
 		if (isset($_SERVER['argv'][1])) {
-			$filename = str_replace('\\', '/', $_SERVER['argv'][1]);
-			$cwd = str_replace('\\', '/', getcwd()) . '/';
-			if (strpos($filename, $cwd) === 0) {
-				$parameters[] = substr($filename, strlen($cwd));
+			$filename = $this->normalizePath($_SERVER['argv'][1]);
+			$cwd = $this->normalizePath(getcwd()) . '/';
+			
+			// IDE always provides absolute path but Codeception only accepts relative path without leading "./".
+			// If path is not absolute, make it that way and call realpath to remove "./".
+			if (strpos($filename, $cwd) !== 0 && file_exists($cwd . $filename)) {
+				$filename = $this->normalizePath(realpath($cwd . $filename));
 			}
+			
+			if (!file_exists($filename)) {
+				echo 'File "' . $filename . '" could not be found.';
+				exit;
+			}
+
+			// Cut of the absolute part for Codeception.
+			$parameters[] = substr($filename, strlen($cwd));
 		}
 
 		parent::__construct($parameters, $definition);
+	}
+
+	private function normalizePath($path)
+	{
+		return str_replace('\\', '/', $path);
 	}
 
 }
