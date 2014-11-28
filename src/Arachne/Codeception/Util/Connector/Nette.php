c4e43ef2a9a964370878d41d14fa683e3a6a554c
Jáchym Toušek <enumag@gmail.com>
2014-11-28 18:42:40 +0100

    Fixed canonicalization

--- src/Arachne/Codeception/Util/Connector/Nette.php
+++ src/Arachne/Codeception/Util/Connector/Nette.php
@@ -40,6 +40,12 @@ class Nette extends Client
 		// Container initialization can't be called earlier because Nette\Http\IRequest service might be initialized too soon and amOnPage method would not work anymore.
 		$this->container->initialize();
 
+		// RequestFactory leaves port NULL in CLI mode but the urls created by amOnPage have port 80 which breaks canonicalization.
+		$url = $this->container->getByType('Nette\Http\IRequest')->getUrl();
+		if (!$url->getPort()) {
+			$url->setPort(80);
+		}
+
 		// The HTTP code from previous test sometimes survives in http_response_code() so it's necessary to reset it manually.
 		$httpResponse = $this->container->getByType('Nette\Http\IResponse');
 		$httpResponse->setCode(IResponse::S200_OK);
