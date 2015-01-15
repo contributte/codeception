cf46cd9480836cfbbca7c63eb19e8e79ec44c6c8
Jáchym Toušek <enumag@gmail.com>
2015-01-16 00:03:05 +0100

    Added basic configuration for tests

--- tests/run.sh
+++ tests/run.sh
@@ -1,1 +1,4 @@
 ./vendor/bin/phpcs -p --standard=vendor/arachne/coding-style/ruleset.xml src
+./vendor/bin/phpcs -p --standard=vendor/arachne/coding-style/ruleset.xml --ignore=_* tests
+./vendor/bin/codecept build
+./vendor/bin/codecept run integration
---
+++ tests/runner.php
@@ -0,0 +1,13 @@
+<?php
+
+use Arachne\Codeception\Console\Input\RunTestInput;
+use Codeception\Codecept;
+use Codeception\Command\Run;
+use Symfony\Component\Console\Application;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = new Application('Codeception', Codecept::VERSION);
+$app->add(new Run('run'));
+
+$app->run(new RunTestInput());
