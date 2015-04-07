5484677d9c76914bbde826e5df5cf483a2518a2f
Jáchym Toušek <enumag@gmail.com>
2015-04-08 00:39:16 +0200

    Fixed DI extension

--- src/DI/CodeceptionExtension.php
+++ src/DI/CodeceptionExtension.php
@@ -2,7 +2,6 @@
 
 namespace Arachne\Codeception\DI;
 
-use Nette\Bridges\Framework\NetteExtension;
 use Nette\DI\CompilerExtension;
 use Nette\Http\IResponse;
 
@@ -16,17 +15,20 @@ class CodeceptionExtension extends CompilerExtension
 	{
 		$builder = $this->getContainerBuilder();
 
-		if ($builder->hasDefinition('httpResponse')) {
-			$builder->getDefinition('httpResponse')
-				->setClass('Arachne\Codeception\Http\Response')
-				// The HTTP code from previous test sometimes survives in http_response_code() so it's necessary to reset it manually.
-				->addSetup('setCode', [ IResponse::S200_OK]);
-		}
-
-		if ($builder->hasDefinition('httpRequest')) {
-			$builder->getDefinition('httpRequest')
+		$request = $builder->getByType('Nette\Http\IRequest') ?: 'httpRequest';
+		if ($builder->hasDefinition($request)) {
+			$builder->getDefinition($request)
 				// RequestFactory leaves port NULL in CLI mode but the urls created by amOnPage have port 80 which breaks canonicalization.
 				->addSetup('$url = ?->getUrl(); if (!$url->getPort()) { $url->setPort(80); }', [ '@self' ]);
+		}
+
+		$response = $builder->getByType('Nette\Http\IResponse') ?: 'httpResponse';
+		if ($builder->hasDefinition($response)) {
+			$builder->getDefinition($response)
+				->setClass('Nette\Http\IResponse')
+				->setFactory('Arachne\Codeception\Http\Response')
+				// The HTTP code from previous test sometimes survives in http_response_code() so it's necessary to reset it manually.
+				->addSetup('setCode', [ IResponse::S200_OK ]);
 		}
 	}
 
