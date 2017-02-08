ab245ca0f7006d3abc8e2260891ceb9fa4662075
Jáchym Toušek <enumag@gmail.com>
2017-02-08 22:46:10 +0100

    Remove copyright headers

--- src/Connector/NetteConnector.php
+++ src/Connector/NetteConnector.php
@@ -1,13 +1,5 @@
 <?php
 
-/*
- * This file is part of the Arachne
- *
- * Copyright (c) Jáchym Toušek (enumag@gmail.com)
- *
- * For the full copyright and license information, please view the file license.md that was distributed with this source code.
- */
-
 namespace Arachne\Codeception\Connector;
 
 use Arachne\Codeception\Http\Request as HttpRequest;
