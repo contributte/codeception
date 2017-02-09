2c31f48dd92bcd3655f1d7f2491704cd939ad0db
Jáchym Toušek <enumag@gmail.com>
2017-02-09 07:38:34 +0100

    Remove Nette\Object

--- tests/functional/src/Fixtures/RouterFactory.php
+++ tests/functional/src/Fixtures/RouterFactory.php
@@ -7,12 +7,11 @@ namespace Tests\Functional\Fixtures;
 use Nette\Application\IRouter;
 use Nette\Application\Routers\Route;
 use Nette\Application\Routers\RouteList;
-use Nette\Object;
 
 /**
  * @author Jáchym Toušek <enumag@gmail.com>
  */
-class RouterFactory extends Object
+class RouterFactory
 {
     /**
      * @return IRouter
