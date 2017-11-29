fe6097814dee80986f4404965fe9dcf7716bb118
Jáchym Toušek <enumag@gmail.com>
2017-11-29 18:29:09 +0100

    Add NetteDIModuleType for PHPStan

---
+++ src/PHPStan/NetteDIModuleType.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arachne\Codeception\PHPStan;
+
+use Arachne\Codeception\Module\NetteDIModule;
+use PhpParser\Node\Expr\ClassConstFetch;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Name;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\DynamicMethodReturnTypeExtension;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\Type;
+
+class NetteDIModuleType implements DynamicMethodReturnTypeExtension
+{
+    public function getClass(): string
+    {
+        return NetteDIModule::class;
+    }
+
+    public function isMethodSupported(MethodReflection $methodReflection): bool
+    {
+        return $methodReflection->getName() === 'grabService';
+    }
+
+    public function getTypeFromMethodCall(MethodReflection $methodReflection, MethodCall $methodCall, Scope $scope): Type
+    {
+        if (!$methodCall->args) {
+            return $methodReflection->getReturnType();
+        }
+
+        $arg = $methodCall->args[0]->value;
+
+        if (!$arg instanceof ClassConstFetch) {
+            return $methodReflection->getReturnType();
+        }
+
+        $class = $arg->class;
+
+        if (!$class instanceof Name) {
+            return $methodReflection->getReturnType();
+        }
+
+        $class = (string) $class;
+
+        if ($class === 'static') {
+            return $methodReflection->getReturnType();
+        }
+
+        if ($class === 'self') {
+            $class = $scope->getClassReflection()->getName();
+        }
+
+        return new ObjectType($class);
+    }
+}
