83879e306cb89cbacfecb8bc65950de142c097f4
Milan Felix Šulc <rkfelix@gmail.com>
2022-10-31 15:10:38 +0100

    Tests: fix for codeception v5

--- tests/Functional/src/Fixtures/RouterFactory.php
+++ tests/Functional/src/Fixtures/RouterFactory.php
@@ -2,14 +2,13 @@
 
 namespace Tests\Functional\Fixtures;
 
-use Nette\Application\IRouter;
 use Nette\Application\Routers\Route;
 use Nette\Application\Routers\RouteList;
 
 class RouterFactory
 {
 
-	public function create(): IRouter
+	public function create(): RouteList
 	{
 		$router = new RouteList();
 		$router[] = new Route('<presenter>[/<action>[/<id>]]', 'Homepage:default');
